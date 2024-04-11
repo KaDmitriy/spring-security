919d8db09c206a4fb68f00014c96465e98178a88
KaDmitriy <ka.dmitriy@gmail.com>
2024-04-11 19:08:31 +0300

    add app: baseapp

---
+++ baseapp/src/test/java/app/kadmitriy/securityserviceapp/baseapp/BaseappApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kadmitriy.securityserviceapp.baseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
